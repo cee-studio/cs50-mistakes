fd326a42c62d771b85982962d644071a69286e2f
Shahroz Ali <58667536+ShahrozAliPK@users.noreply.github.com>
2020-07-12 12:51:16 +0500

    fread() function for recover.c
    A demonstration for fread() function and how to read 512 bytes of block

---
+++ fread.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+    // Open card.raw file in read mode
+    FILE *fp = fopen(argv[1], "r");
+
+    // Check for NULL
+    if (!fp)
+    {
+        printf("Error\n");
+        fclose(fp);
+        return 1;
+    }
+
+    // Storage to store 1 block
+    unsigned char buffer[512];
+
+    // Loop till EOF (end of the file)
+    while (fread(buffer, sizeof(unsigned char), 512, fp) != 0)
+    {
+        // Your code goes here
+    }
+
+    // End the program
+    fclose(fp);
+    return 0;
+}
