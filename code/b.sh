da4d8740ff362c6db011a09f80dcc105040814a1
mlite <mlite@users.noreply.github.com>
2020-10-13 05:32:19 -0700

    add suffixes

--- code/b.sh
+++ code/b.sh
@@ -5,8 +5,8 @@ for i in *; do
     if [ -d $i ]; then
         echo "building $i .."
         cd $mypath/$i
-        clang *.c -g -lcs50 -o b
-        clang *.c -fsanitize=address -g -lcs50 -o b.asan
+        clang *.c -g -lcs50 -o b.exe
+        clang *.c -fsanitize=address -g -lcs50 -o b.asan.exe
         cd $mypath
     fi
 done
