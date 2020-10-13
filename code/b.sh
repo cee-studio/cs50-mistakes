dfd474a81d86c9ff2f4807430bb35ea0a4c4357b
mlite <mlite@users.noreply.github.com>
2020-10-13 05:13:19 -0700

    add the building script

--- code/b.sh
+++ code/b.sh
@@ -3,7 +3,7 @@ mypath=$(dirname $(readlink -f $0))
 
 for i in *; do
     if [ -d $i ]; then
-        echo "building .."
+        echo "building $i .."
         cd $mypath/$i
         clang *.c -g -lcs50 -o b
         clang *.c -fsanitize=address -g -lcs50 -o b.asan
